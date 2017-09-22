f8e5acc8af49fc5ef97c56ec529beaff6077adae
ntwo1980 <ntwo1980@gmail.com>
2017-09-22 10:24:35 +0800

    update

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import datetime
 import subprocess
 import shlex
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import XueQiu
 import JoinQuant
@@ -49,23 +50,23 @@ def generate_blog_up_down(generator):
     generator.line('五日内创一个月新低比例{:.2f}%, 五日内创一个月新高比例{:.2f}%。股价低于一个月前股价95%比例{:.2f}%，股价高于一个前股价95%比例{:.2f}。'
                    .format(df['low'][-1], df['high'][-1], df['down'][-1], df['up'][-1]))
 
-    fig, axes = plt.subplots(2, 1, figsize=(16, 18))
+    fig, axes = plt.subplots(2, 1, figsize=(16, 12))
 
     ax1 = axes[0]
-    ax1.plot(df.index, df['low'], label='新低比例')
-    ax1.plot(df.index, df['high'], label='新高比例')
+    ax1.plot(df.index, df['low'], label='low pct')
+    ax1.plot(df.index, df['high'], label='high pct')
     ax1.legend(loc='upper left')
     ax1.set_ylabel('low/high pct')
     ax3= ax1.twinx()
-    ax3.plot(df.index, df['index'], 'y', label='深成指')
+    ax3.plot(df.index, df['index'], 'y', label='399001')
 
     ax2 = axes[1]
-    ax2.plot(df.index, df['down'], label='下跌比例')
-    ax2.plot(df.index, df['up'], label='上涨比例')
+    ax2.plot(df.index, df['down'], label='down pct')
+    ax2.plot(df.index, df['up'], label='up pct')
     ax2.legend(loc='upper left')
     ax2.set_ylabel('down/up pct')
     ax4= ax2.twinx()
-    ax4.plot(df.index, df['index'], 'y', label='深成指' )
+    ax4.plot(df.index, df['index'], 'y', label='399001' )
 
     figure_name = ('r_up_down_{}.png').format(today_str)
     figure_path = '{}{}'.format(blog_upload_absolute_path, figure_name)

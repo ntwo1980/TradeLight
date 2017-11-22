bd45cfb981bce2c8f524d5c00828ef12160cfb73
Luke Ni <ning.ni@commscope.com>
2017-11-22 16:49:04 +0800

    update

--- jobs/SWDownloadFilesJob.py
+++ jobs/SWDownloadFilesJob.py
@@ -1,4 +1,5 @@
 import jobs.JobBase as j
+from bs4 import BeautifulSoup
 
 class SWDownloadFilesJob(j.JobBase):
     def __init__(self, sw, download_files):
@@ -7,4 +8,35 @@ class SWDownloadFilesJob(j.JobBase):
 
     def run(self):
         for f in self.download_files:
-            self.sw.fetch_file(f, 'r_sw_' + f)
+            html = self.sw.fetch_file(f)
+            items = []
+
+            for tr in html.find_all("tr")[1:]:
+                children = list(tr.children)
+                d = dt.datetime.strptime(children[2].string, "%Y/%m/%d %H:%M:%S")
+                dict = {'Code': children[0].string,
+                    'Name': children[1].string,
+                    'Date': d,
+                    'Close': children[3].string,
+                    'Volumn': children[4].string,
+                    'Change': children[5].string,
+                    'Turnover': children[6].string,
+                    'PE': children[7].string,
+                    'PB': children[8].string,
+                    'Average': children[9].string,
+                    'AmountPercentage': children[10].string,
+                    'HQLTSZ': children[11].string.replace(',', ''),
+                    'AHQLTSZ': children[12].string.replace(',', ''),
+                    'Payout': children[13].string}
+                # Code,Name,Date,Open,High,Low,Close,Volumn,Amount,Change,Turnover,PE,PB,Average,AmountPercentage,HQLTSZ,AHQLTSZ,Payout
+                item_str = '{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(
+                            dict["Code"], dict["Name"], dict["Date"].strftime("%m/%d/%Y"), '', '', '',
+                            dict["Close"], dict["Volumn"], '',
+                            dict["Change"], dict["Turnover"], dict["PE"], dict["PB"],
+                            dict["Average"], dict["AmountPercentage"],
+                            dict["HQLTSZ"], dict["AHQLTSZ"], dict["Payout"])
+                items.append(item_str)
+
+            with open('r_sw_{}.csv'.format(f), 'w', encoding="utf-8") as file:
+                    for i in reversed(items):
+                        file.write(i + "\n")

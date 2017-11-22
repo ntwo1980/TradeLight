91c3e19f13ec6384ab08b72621296f151670c8c0
Luke Ni <ning.ni@commscope.com>
2017-11-22 12:50:38 +0800

    update

---
+++ jobs/SWDowloadFilesJob.py
@@ -0,0 +1,10 @@
+import jobs.JobBase as j
+
+class SWDownloadFilesJob(j.JobBase):
+    def __init__(self, sw, download_files):
+        self.sw = sw
+        self.download_files = download_files
+
+    def run(self):
+        for f in self.download_files:
+            sw.fetch_file(f, 'r_sw_' + f)

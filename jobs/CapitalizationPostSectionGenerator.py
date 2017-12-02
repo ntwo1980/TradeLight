774425423383be3cccc3ebb27c731bee4456b840
ntwo1980 <ntwo1980@gmail.com>
2017-12-03 06:45:46 +0800

    update

--- jobs/CapitalizationPostSectionGenerator.py
+++ jobs/CapitalizationPostSectionGenerator.py
@@ -23,6 +23,7 @@ class CapitalizationPostSectionGenerator(p.PostSectionGenerator):
         for f in csv_files]
 
         for factor in ['PB', 'PE']:
+            blog_generator.h4(factor)
             for i in range(0, 3):
                 df = dfs[i]
                 capitalization_group_name = capitalization_group_names[i]
@@ -37,18 +38,31 @@ class CapitalizationPostSectionGenerator(p.PostSectionGenerator):
                     float(stats.percentileofscore(df[factor].iloc[-1200:], last_factor_value)),
                     float(stats.percentileofscore(df[factor].iloc[-2400:], last_factor_value))))
 
-                fig, axes = plt.subplots(1, 1, figsize=(16, 6))
+                for year in [1, 10]:
+                    days = 240 * year
+                    figure_name = 'r_{}_{}_{}.png'.format(df['Code'].iloc[0], factor, str(year))
+                    self.generate_factor_figure(
+                        figure_name,
+                        df['Date'].iloc[-days:],
+                        df[factor].iloc[-days:],
+                        factor,
+                        df['Close'].iloc[-days:],
+                        'Close'
+                    )
+                    blog_generator.img('{}{}'.format(self.blog_upload_relative_path, figure_name))
 
-                ax1 = axes
-                ax1.plot(df['Date'].iloc[-2400:], pd.to_numeric(df[factor].iloc[-2400:], 'coerce', 'float'), label=factor)
-                ax1.legend(loc='upper left')
-                ax1.set_ylabel(factor)
-                ax2= ax1.twinx()
-                ax2.plot(df['Date'].iloc[-2400:], pd.to_numeric(df['Close'].iloc[-2400:], 'coerce', 'float'), label='Close', color='orange')
+    def generate_factor_figure(self, figure_name, data_x, data_y1, label_y1, data_y2 = None, label_y2=None):
+        fig, axes = plt.subplots(1, 1, figsize=(16, 6))
 
-                figure_name = 'r_{}_{}.png'.format(df['Code'].iloc[0], factor)
-                figure_path = '{}{}'.format(self.blog_upload_absolute_path, figure_name)
+        ax1 = axes
+        ax1.plot(data_x, pd.to_numeric(data_y1, 'coerce', 'float'), label=label_y1)
+        ax1.legend(loc='upper left')
+        ax1.set_ylabel(label_y1)
 
-                plt.savefig(figure_path, bbox_inches='tight')
+        if data_y2 is not None:
+            ax2= ax1.twinx()
+            ax2.plot(data_x, pd.to_numeric(data_y2, 'coerce', 'float'), label='Close', color='orange')
 
-                blog_generator.img('{}{}'.format(self.blog_upload_relative_path, figure_name))
+        figure_path = '{}{}'.format(self.blog_upload_absolute_path, figure_name)
+
+        plt.savefig(figure_path, bbox_inches='tight')

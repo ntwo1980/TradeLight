ef39b464e7a01bc8b99b0c4ac4e6a41f63da3b52
Luke Ni <ning.ni@commscope.com>
2017-12-10 15:55:28 +0800

    update

--- jobs/SWStatPostSectionGenerator.py
+++ jobs/SWStatPostSectionGenerator.py
@@ -189,7 +189,7 @@ class SWStatPostSectionGenerator(p.PostSectionGenerator):
             header=None, names=columns, parse_dates=['Date'],
             infer_datetime_format=True)
 
-        df = df[(df['PB']!='None')&(df['PE']='None')]
+        df = df[(df['PB']!='None')&(df['PE']!='None')]
         df['ROE'] = df['PB'][df['PB']!='None'].astype('float') / df ['PE'][df['PE']!='None'].astype('float')
 
         return df

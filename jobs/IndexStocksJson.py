7484c631557b608d1e58644ad90c4dbba17d8067
ntwo1980 <ntwo1980@gmail.com>
2018-01-06 11:27:01 +0800

    update

--- jobs/IndexStocksJson.py
+++ jobs/IndexStocksJson.py
@@ -1,8 +1,9 @@
+import os
 import datetime
 import numpy as np
 import pandas as pd
+import json
 import jobs.JobBase as j
-import HexoGenerator
 
 class IndexStocksJson(j.JobBase):
     def __init__(self, json_dir, index_file_path):
@@ -11,5 +12,15 @@ class IndexStocksJson(j.JobBase):
 
     def run(self):
         df = pd.read_csv(self.index_file_path)
-        print(df)
+        index_json = dict()
 
+        for index, group in df.groupby('index'):
+            data = dict()
+            data['name'] = group['index_name'].iloc[0]
+            data['stocks'] = list(group['stock'].str[:6])
+
+            index_json[index[0:6]] = data
+
+        print(os.path.join(self.json_dir, 'index_stocks.json'))
+        with open(os.path.join(self.json_dir, 'index_stocks.json'), 'w') as f:
+            json.dump(index_json, f)

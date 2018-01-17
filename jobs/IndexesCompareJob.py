305724174d95a1577e2135af18c5b2ae3160b38b
Luke Ni <ning.ni@commscope.com>
2018-01-17 10:51:24 +0800

    update

--- jobs/IndexesCompareJob.py
+++ jobs/IndexesCompareJob.py
@@ -5,9 +5,9 @@ import jobs.PostSectionGenerator as p
 from scipy import stats
 
 class IndexesCompareJob(p.PostSectionGenerator):
-    def __init__(self, index_closes_file_path, index_names_files_path, blog_upload_relative_path, blog_upload_absolute_path):
+    def __init__(self, index_closes_file_path, index_names_file_path, blog_upload_relative_path, blog_upload_absolute_path):
         self.index_closes_file_path = index_closes_file_path
-        self.index_names_files_path = index_names_files_path
+        self.index_names_file_path = index_names_file_path
         self.blog_upload_relative_path = blog_upload_relative_path
         self.blog_upload_absolute_path = blog_upload_absolute_path
 
@@ -15,11 +15,11 @@ class IndexesCompareJob(p.PostSectionGenerator):
         blog_generator.h3('指数对比')
 
         df_names = pd.read_csv(
-            index_names_files_path
+            self.index_names_file_path,
             header=True,
             infer_datetime_format=True)
 
         df_closes = pd.read_csv(
-            index_closes_file_path,
+            self.index_closes_file_path,
             header=True, parse_dates=['date'],
             infer_datetime_format=True)

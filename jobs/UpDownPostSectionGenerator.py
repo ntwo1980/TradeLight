fb1250d647d57e566d8739feb5fdd966fd20bd1d
Luke Ni <ning.ni@commscope.com>
2018-02-07 16:31:26 +0800

    update

--- jobs/UpDownPostSectionGenerator.py
+++ jobs/UpDownPostSectionGenerator.py
@@ -9,10 +9,9 @@ class UpDownPostSectionGenerator(p.PostSectionGenerator):
         self.blog_upload_absolute_path = blog_upload_absolute_path
 
     def generate(self, blog_generator):
-        blog_generator.h3('新低新高')
-        # data_path = os.path.join(script_dir, 'data/r_up_down.csv')
-
         df = pd.read_csv(self.data_file_path, index_col='date', parse_dates=True)
+
+        blog_generator.h3('新低新高')
 
         blog_generator.line('五日内创一个月新低比例{:.2f}%, 五日内创一个月新高比例{:.2f}%。股价低于一个月前股价95%比例{:.2f}%，股价高于一个前股价95%比例{:.2f}%。'
                     .format(df['low'][-1], df['high'][-1], df['down'][-1], df['up'][-1]))
@@ -36,6 +35,28 @@ class UpDownPostSectionGenerator(p.PostSectionGenerator):
         ax4.plot(df.index, df['index'], 'y', label='399001' )
 
         figure_name = ('r_up_down.png')
+        figure_path = '{}{}'.format(self.blog_upload_absolute_path, figure_name)
+
+        plt.savefig(figure_path, bbox_inches='tight')
+
+        blog_generator.img('{}{}'.format(self.blog_upload_relative_path, figure_name))
+
+        blog_generator.h3('涨跌停')
+
+        blog_generator.line('涨幅超过9%比例{:.2f}%, 跌幅超过9%比例{:.2f}%。'
+                    .format(df['great_up'][-1], df['great_down'][-1]))
+
+        fig, axes = plt.subplots(1, 1, figsize=(16, 12))
+
+        ax1 = axes[0]
+        ax1.plot(df.index, df['great_up'], label='great up pct')
+        ax1.plot(df.index, df['great_down'], label='great down pct')
+        ax1.legend(loc='upper left')
+        ax1.set_ylabel('up/down pct')
+        ax2= ax1.twinx()
+        axw.plot(df.index, df['index'], 'y', label='399001')
+
+        figure_name = ('r_great_up_down.png')
         figure_path = '{}{}'.format(self.blog_upload_absolute_path, figure_name)
 
         plt.savefig(figure_path, bbox_inches='tight')

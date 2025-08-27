72fe3feca10786175325f45b1eaa961c6ecfb7ae
luke <ntwo1980@gmail.com>
2025-08-27 15:33:34 +0800

    add trades

--- JoinQuant.py
+++ JoinQuant.py
@@ -4,24 +4,23 @@ import json
 import re
 import requests
 import unittest
-import pandas as pd
 
 class JoinQuant:
     WEB_ROOT = 'https://www.joinquant.com'
     LOGIN_PAGE = WEB_ROOT
-    LOGIN_API =  '{}/user/login/doLogin?ajax=1'.format(WEB_ROOT)
+    LOGIN_API =  '{}/user/login/doLoginByText'.format(WEB_ROOT)
     TRANSACTION_API = '{}/algorithm/live/transactionDetail'.format(WEB_ROOT)
     WEB_REFERER = '{}/user/login/index'.format(WEB_ROOT)
     WEB_ORIGIN = WEB_ROOT
     JUPTER_PAGE = '{}/hub/login?next='.format(WEB_ROOT)
-    DOWNLOAD_FILES = WEB_ROOT + '/user/{}/files/data/{}?download=1'
+    DOWNLOAD_FILES = WEB_ROOT + '/user/{}/files/{}?download=1'
 
     def __init__(self, config):
         self.s = requests.Session()
         self.config = config
 
         self.script_dir = os.path.dirname(__file__)
-        self.data_path = os.path.join(self.script_dir, 'data/')
+        self.data_path = os.path.join(self.script_dir, '')
 
     def login(self):
         # mock headers
@@ -43,15 +42,15 @@ class JoinQuant:
         self.s.get(self.LOGIN_PAGE)
 
         # post for login
-        account_config = self.config['account']
+        # account_config = self.config['account']
         params = self.create_login_params(
-            account_config['user'],
-            account_config['password'])
+            '13962143063',
+            'hVk94f0rcL')
 
         rep = self.s.post(self.LOGIN_API, data=params)
-        set_cookie = rep.headers['Set-Cookie']
-        if len(set_cookie) < 100:
-            return False, 'Login failed'
+        # set_cookie = rep.headers['Set-Cookie']
+        # if len(set_cookie) < 100:
+        #     return False, 'Login failed'
 
         # self.s.headers.update({
             # 'cookie': set_cookie
@@ -59,7 +58,7 @@ class JoinQuant:
 
         # init jupter cookie
         params = {
-            'username': account_config['userid'],
+            'username': '23286199053',
             'token': self.s.cookies['PHPSESSID']
         }
         rep = self.s.post(self.JUPTER_PAGE, data=params)
@@ -75,8 +74,9 @@ class JoinQuant:
         return params
 
     def fetch_file(self, file_name, new_file_name):
-        account_config = self.config['account']
-        rep = self.s.get(self.DOWNLOAD_FILES.format(account_config['userid'], file_name))
+        # account_config = self.config['account']
+        print(self.DOWNLOAD_FILES.format('23286199053', file_name))
+        rep = self.s.get(self.DOWNLOAD_FILES.format('23286199053', file_name))
 
         if not new_file_name is None:
             file_path = os.path.join(self.data_path, new_file_name)
@@ -88,12 +88,12 @@ class JoinQuant:
 class JoinQuantTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        script_dir = os.path.dirname(__file__)
-        config_file_path = os.path.join(script_dir, 'config/JoinQuant.json')
-        with open(config_file_path, 'r') as f:
-            config = json.load(f)
+        # script_dir = os.path.dirname(__file__)
+        # config_file_path = os.path.join(script_dir, 'config/JoinQuant.json')
+        # with open(config_file_path, 'r') as f:
+        #     config = json.load(f)
 
-        self.jq = JoinQuant(config)
+        self.jq = JoinQuant(None)
         succeed, reason = self.jq.login()
         if not succeed:
             raise Exception("login failed: " + reason)
@@ -103,7 +103,7 @@ class JoinQuantTest(unittest.TestCase):
         pass
 
     def test_downlad_file(self):
-        self.jq.fetch_file('up_down.csv', 'r_up_down.csv')
+        self.jq.fetch_file('trade.txt', 'trade.txt')
 
 if __name__ == '__main__':
     unittest.main()

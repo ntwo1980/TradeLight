eb86f78aec5138d2c6614206b89dd1bf0a7c1230
luke <ntwo1980@gmail.com>
2026-03-08 08:20:32 +0800

    double first position edge condition

--- jz/Strategy/用户策略/Strategy.py
+++ jz/Strategy/用户策略/Strategy.py
@@ -832,7 +832,7 @@ class SpreadGridStrategy(BaseStrategy):
                         if self.double_first_position and self.logical_holding <= orderQty * 2:
                             orderQuantity = orderQty * 2
 
-                        if self.logical_holding == orderQuantity and buy_position == orderQuantity:
+                        if self.logical_holding <= orderQuantity and buy_position <= orderQuantity:
                             if current_price >= sum(close_prices[-20:]) / 20 - self.atr * self.buy_levels[0]:
                                 executed = self.ExecuteSell(self.codes[1], current_price, orderQuantity, True)
                         else:
@@ -870,7 +870,7 @@ class SpreadGridStrategy(BaseStrategy):
                         if self.double_first_position and abs(self.logical_holding) <= orderQty * 2:
                             orderQuantity = orderQty * 2
 
-                        if self.logical_holding == -orderQuantity and sell_position == orderQuantity:
+                        if abs(self.logical_holding) <= orderQuantity and sell_position <= orderQuantity:
                             if current_price <= sum(close_prices[-20:]) / 20 + self.atr * self.sell_levels[0]:
                                 executed = self.ExecuteBuy(self.codes[1], current_price, orderQuantity, False, True)
                         else:
